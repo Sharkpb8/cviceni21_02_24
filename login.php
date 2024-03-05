f639af14e54e6cf2ecd30e582b7a15fe762770a0
Sharkpb8 <sharkpb8@gmail.com>
2024-03-05 18:20:09 +0100

    login works

--- login.php
+++ login.php
@@ -7,6 +7,12 @@ if(empty($_POST["username"]) || empty($_POST["password"])){
     exit();
 }
 
+$query = DBC::getConnection()->query("select username, password from uzivatel where username = '" . $_POST["username"] . "' and password = '" . $_POST["password"] . "';");
+
+if ($query->num_rows <= 0)
+{
+    die();
+}
 $username = $_POST["username"];
 $_SESSION['username'] = $username;
 $_SESSION["loggedin"] = true;
