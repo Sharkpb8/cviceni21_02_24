44ee6fc5053aab9f4a90015d3d588d172a2a6ce2
Sharkpb8 <sharkpb8@gmail.com>
2024-03-18 17:56:17 +0100

    login works

--- login.php
+++ login.php
@@ -7,7 +7,9 @@ if(empty($_POST["username"]) || empty($_POST["password"])){
     exit();
 }
 
-$hash = password_hash($_POST["password"],PASSWORD_DEFAULT);
+verifyUser($_POST["username"], $_POST["password"]);
+
+/* $hash = password_hash($_POST["password"],PASSWORD_DEFAULT);
 $query = DBC::getConnection()->query("select username, password from uzivatel where username = '" . $_POST["username"] . "' and password = '" . $hash . "';");
 
 $match = false;
@@ -20,12 +22,23 @@ while($row = $query->fetch_assoc()) {
 }
 if(!$match){
     die();
+} */
+
+function verifyUser(string $username, string $password): void
+{
+    $connection = DBC::getConnection();
+    $statement = $connection->prepare("SELECT id, username, password FROM uzivatel WHERE username = :username LIMIT 1");
+    $statement->execute([":username" => $username]);
+    $result = $statement->fetch(PDO::FETCH_ASSOC);
+    if ($result && password_verify($password, $result["password"])) {
+        $_SESSION["user_id"] = $result["id"];
+        $_SESSION["user_name"] = $result["username"];
+        header("Location: /");
+    } else {
+        header("Location: /login");
+    }
 }
 
-/* if ($query->num_rows <= 0)
-{
-    die();
-} */
 
 $username = $_POST["username"];
 $_SESSION['username'] = $username;
